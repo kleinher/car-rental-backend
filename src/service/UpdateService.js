5abc5a5daf7a930e4b888c81e6490ea24eb7fdf9
Hernan Kleinubing <hernan.k97@gmail.com>
2025-01-18 15:08:28 +0100

    actualizado funcionamiento de cache

--- src/service/UpdateService.js
+++ src/service/UpdateService.js
@@ -2,46 +2,56 @@ const logger = require('../config/logger');
 const { updateCar, getCarByPlateAndPhone } = require('../client/CarsClient');
 const { broadcast } = require('../websocket/WebSocketServer.js');
 
-const cache = new Map();
+const phoneCache = new Map();
+
 
 async function setDataWithCache(phoneNumber, licencePlate) {
-    if (cache.has(phoneNumber)) {
-        logger.info("Cache hit:" + phoneNumber);
-        return;
+    if (!phoneCache.has(phoneNumber)) {
+        phoneCache.set(phoneNumber, new Set());
     }
-    cache.set(phoneNumber, licencePlate);
-    let car = getCarByPlateAndPhone(cache.get(phoneNumber), phoneNumber);
+    phoneCache.get(phoneNumber).add(licencePlate);
+
+    let car = getCarByPlateAndPhone(licencePlate, phoneNumber);
     car.reminderSent = true;
     car.reminderSentDate = new Date();
     updateCar(car);
     broadcast();
 }
 
+
 async function updateDataWithCache(phoneNumber, message) {
-    if (cache.has(phoneNumber)) {
-        if (!isNaN(message)) {
-            let car = getCarByPlateAndPhone(cache.get(phoneNumber), phoneNumber);
-
-            if (car.kilometers > message) {
-                logger.error("Kilometers are not greater than the previous one");
-                return;
-            }
-
-            car.inMaintenance = (message - car.kilometers) > 10000 ? true : false;
-            car.kilometers = message;
-            car.reminderSent = false;
-            car.reminderSentDate = null;
-            cache.delete(phoneNumber);
-            updateCar(car);
-            broadcast();
-            logger.info("Es numero:" + message);
-
-        } else {
-            logger.error("Message is not a number: " + message);
-        }
+    if (!phoneCache.has(phoneNumber)) {
+        logger.info("Cache miss:" + phoneNumber);
         return;
     }
-    logger.info("Cache miss:" + phoneNumber);
+
+    if (isNaN(message)) {
+        logger.error("Message is not a number: " + message);
+        return;
+    }
+
+    let licencePlates = phoneCache.get(phoneNumber);
+    let licencePlate = licencePlates.values().next().value; // Get the first licence plate
+    let car = getCarByPlateAndPhone(licencePlate, phoneNumber);
+
+    if (car.kilometers > message) {
+        logger.error(`Kilometers for ${licencePlate} are not greater than the previous one`);
+        return;
+    }
+
+    car.inMaintenance = (message - car.kilometers) > 10000 ? true : false;
+    car.kilometers = message;
+    car.reminderSent = false;
+    car.reminderSentDate = null;
+    car.lastUpdated = new Date();
+    updateCar(car);
+    broadcast();
+    logger.info(`Updated car with licence plate ${licencePlate} for phone number ${phoneNumber}`);
+
+    licencePlates.delete(licencePlate); // Remove the licence plate from the set
+    if (licencePlates.size === 0) {
+        phoneCache.delete(phoneNumber); // Remove the phone number from the cache if no licence plates are left
+    }
 }
 
 module.exports = {
