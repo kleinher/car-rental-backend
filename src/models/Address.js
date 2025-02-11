76a07ce79917b8b0892cfbed44d9c50893257f27
Mica <micaeladelahera@gmail.com>
2025-02-11 14:06:32 +0100

    Address:quitada relacion con Driver

--- src/models/Address.js
+++ src/models/Address.js
@@ -1,6 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/sequialize');
-const Driver = require('./Driver'); // Importar aquí después de definir Driver
 
 class Address extends Model { }
 
@@ -22,16 +21,6 @@ Address.init({
     modelName: 'Address',
     tableName: 'addresses',
     timestamps: false
-});
-
-Driver.hasOne(Address, {
-    foreignKey: 'addressId',
-    as: 'address'
-});
-
-Address.belongsTo(Driver, {
-    foreignKey: 'addressId',
-    as: 'driver'
 });
 
 module.exports = Address;
