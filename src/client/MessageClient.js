c208c04a25dfbdb05f4031e29ab4dc3d7cb2208f
Hernan Kleinubing <hernan.k97@gmail.com>
2025-02-16 20:10:54 +0100

    Add detailed logging for message sending in MessageClient

--- src/client/MessageClient.js
+++ src/client/MessageClient.js
@@ -2,11 +2,23 @@ const { client } = require('./WhatsappClient');
 const logger = require('../config/logger'); // Asegúrate de tener configurado tu logger
 
 async function sendMessage(number, message) {
-    logger.info(`Enviando mensaje por wpp cliente: ${client}`)
+    logger.info(`📨 Intentando enviar mensaje a ${number}`);
+
+    logger.info("Client estado" + await client.getState())
+    if (!client || !client.info || !client.info.wid) {
+        logger.error("❌ El cliente de WhatsApp no está conectado.");
+        throw new Error("El cliente de WhatsApp no está autenticado.");
+    }
+
     const chatId = `${number}@c.us`;
-    return client.sendMessage(chatId, message);
+    try {
+        logger.info(`✅ Cliente de WhatsApp conectado. Enviando mensaje...`);
+        await client.sendMessage(chatId, message);
+        logger.info(`📩 Mensaje enviado a ${number}`);
+    } catch (error) {
+        logger.error(`❌ Error enviando mensaje: ${error.message}`);
+        throw error;
+    }
 }
 
-module.exports = {
-    sendMessage,
-};
+module.exports = { sendMessage };
