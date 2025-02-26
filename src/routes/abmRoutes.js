7eafb5708314d947ae05bc9737b5b615cc30b8e3
Hernan Kleinubing <hernan.k97@gmail.com>
2025-02-26 20:33:39 +0100

    Remove abmRoutes and related endpoint for car maintenance completion

--- src/routes/abmRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const abmRouter = express.Router();
-const { createNew } = require('../controllers/GenericController');
-
-abmRouter.post('/create/end', (req, res) => createNew(req, res));
-module.exports = abmRouter;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,5 @@ const NotificationController = require('../controllers/NotificationController');
 const { carEndMaintenance } = require('../controllers/CarsController');
 
 router.post('/send', (req, res) => NotificationController.sendNotification(req, res));
-router.post('/car/maintainance/end', (req, res) => carEndMaintenance(req, res));
 
 module.exports = router;
